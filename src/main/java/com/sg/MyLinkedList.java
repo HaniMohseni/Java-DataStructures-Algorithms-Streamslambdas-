dceb57f34548b82d274c24d78a95bd0074a04103
Hanieh <mohseni.hanieh@gmail.com>
2023-02-05 01:25:42 -0500

    Add LinkedList with unit test

--- src/main/java/com/sg/MyLinkedList.java
+++ src/main/java/com/sg/MyLinkedList.java
@@ -1,4 +1,8 @@
 package com.sg;
 
+import java.util.List;
+
 public interface MyLinkedList {
+    void addToLinkedList(List<String>input,String item);
+    List createSynchronized(List<String>input);
 }
